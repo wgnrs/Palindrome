208f0e4125a5db4d389557d78eefef2983a4b0d7
Wagner <wagner_silvajr@hotmail.com>
2022-07-28 19:51:28 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int originalNumber = 0;
+            int tempNumber = 0;
+            int remainder = 0;
+            int reverseNumber = 0;
+
+            System.Console.WriteLine("Input a 4 digit number : ");
+            originalNumber = int.Parse(Console.ReadLine());
+            System.Console.WriteLine();
+
+            tempNumber = originalNumber;
+            
+            while (originalNumber > 0)
+            {
+                remainder = originalNumber % 10;                
+                reverseNumber = reverseNumber * 10 + remainder;                
+                originalNumber /= 10;
+                
+            }
+            
+
+            if (tempNumber == reverseNumber)
+            {
+                Console.WriteLine("Entered number is a palindrome number");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is not a palindrome number");
+            }                        
+            Console.ReadLine();
+        }
+    }
+}
