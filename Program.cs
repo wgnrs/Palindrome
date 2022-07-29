a2651454029439ddb280e620c655e00ae7121b80
Wagner <wagner_silvajr@hotmail.com>
2022-07-29 18:58:35 -0300

    fix: implemented try catch to deal with exceptions

--- Program.cs
+++ Program.cs
@@ -12,28 +12,31 @@ namespace PalindromeNumber
             int remainder = 0;
             int reverseNumber = 0;
 
-            System.Console.WriteLine("Input a 4 digit number : ");
-            originalNumber = int.Parse(Console.ReadLine());
-            System.Console.WriteLine();
+            try
+            {
+                System.Console.WriteLine("Input a 4 digit number : ");
+                originalNumber = int.Parse(Console.ReadLine());
+                System.Console.WriteLine();
 
-            tempNumber = originalNumber;
-            
-            while (originalNumber > 0)
-            {
-                remainder = originalNumber % 10;                
-                reverseNumber = reverseNumber * 10 + remainder;                
-                originalNumber /= 10;                
+                tempNumber = originalNumber;
+
+
+                while (originalNumber > 0)
+                {
+                    remainder = originalNumber % 10;
+                    reverseNumber = reverseNumber * 10 + remainder;
+                    originalNumber /= 10;
+                }
+                if (tempNumber == reverseNumber)
+                    Console.WriteLine("Entered number is a palindrome number");
+                else
+                    Console.WriteLine("Entered number is not a palindrome number");
+
             }
-            
-            if (tempNumber == reverseNumber)
+            catch (FormatException e)
             {
-                Console.WriteLine("Entered number is a palindrome number");
+                System.Console.WriteLine(e.Message);
             }
-            else
-            {
-                Console.WriteLine("Entered number is not a palindrome number");
-            }                        
-            Console.ReadLine();
         }
     }
 }
